760ee37caa001e66776c79059163c2258b526bf1
Yunchao "Lance" Liu <lanceknight26@gmail.com>
2021-02-19 11:34:57 -0600

    add path to look for other local modules

--- main/main_cv.py
+++ main/main_cv.py
@@ -1,4 +1,7 @@
 from __future__ import print_function
+import sys, os
+path = os.path.abspath(os.path.join('../..', ''))
+sys.path.append(path)
 from conv_qsar_fast.utils.parsing import input_to_bool
 from conv_qsar_fast.utils.parse_cfg import read_config
 import conv_qsar_fast.utils.reset_layers as reset_layers
